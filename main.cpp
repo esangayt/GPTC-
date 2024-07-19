9f1cefcd0a1087c4151520e18913a5f1ab3310be
esangayt <24_de_7@protonmail.com>
2024-07-19 15:22:44 -0500

    Punteros y funciones

--- main.cpp
+++ main.cpp
@@ -1,39 +1,50 @@
 #include <iostream>
+void saludar() {
+    std::cout << "Hola" << std::endl;
+}
 
+void saludarNombre(std::string nombre) {
+    std::cout << "Hola " << nombre << std::endl;
+}
+
+int sumar(int a, int b) {
+    return a + b;
+}
+
+int multi(int a, int b=1) {
+    return a*b;
+}
+
+void pasaPorValor(int x) {
+    x = x +1;
+}
+
+void porReferencia(int &x) {
+    x = x +1;
+}
+
+int* crearArray(int tamaño) {
+    int* array = new int[tamaño];
+    for (int i=0; i<tamaño; i++) {
+        array[i] = i*2;
+    }
+    return  array;
+}
 
 int main() {
-    // Tipos de datos primitivos
-    int entero = 5;
-    float flotant = 3.15f;
-    double doble = 3.1416;
-    char caracter = 'a';
-    bool booleano = true;
+    // Funciones
+    saludar();
+    saludarNombre("Erlin");
+    std::cout << "Suma " <<  sumar(1,2) << std::endl;
+    std::cout << "Suma " <<  multi(2) << std::endl;
 
-    std::cout << "Entero: " << entero << std::endl;
-    std::cout << "Flotante: " << flotant << std::endl;
-    std::cout << "Doble: " << doble << std::endl;
-    std::cout << "Caracter: " << caracter << std::endl;
-    std::cout << "Booleano: " << booleano << std::endl;
+    int numero = 5;
+    pasaPorValor(numero);
+    std::cout << "Número después de incrementar por valor: " << numero << std::endl; // Salida: 5
 
-    //call OperadoresRelacionales
-    int x = 5;
-    int y = 10;
-
-    std::cout << (x == y) << std::endl;
-    std::cout << (x != y) << std::endl;
-    std::cout << (x > y) << std::endl;
-    std::cout << (x < y) << std::endl;
-    std::cout << (x >= y) << std::endl;
-    std::cout << (x <= y) << std::endl;
-
-    //Operadores lógicos
-    bool a = true;
-    bool b = false;
-
-    std::cout << (a && b) << std::endl;
-    std::cout << (a || b) << std::endl;
-    std::cout << (!a) << std::endl;
-
-    //Estructuras condicionales
-
+    int numero2=6;
+    porReferencia(numero2);
+    std::cout << "Número después de incrementar por referencia: " << numero2 << std::endl; // Salida: 7
+    return 0;
 }
+
